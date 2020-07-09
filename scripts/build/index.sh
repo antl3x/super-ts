cf1ee67fafdfad415265eec7083b6c96bb158b43
nthypes <aasm.moura@gmail.com>
2020-07-09 11:57:08 -0300

    chore(super-ts): updates prepublish and test script

--- scripts/build/index.sh
+++ scripts/build/index.sh
@@ -1,9 +1,11 @@
 # Build CJS Modules
+rm -rf cjs
 node_modules/.bin/tsc -p tsconfig.build.json
 node_modules/.bin/ef-tspm -c tsconfig.build.json
 rm -rf cjs/tsconfig.build.tsbuildinfo
 
 # Build ES Modules
+rm -rf esm
 node_modules/.bin/tsc -p tsconfig.esm.json
 node_modules/.bin/ef-tspm -c tsconfig.esm.json
 find ./esm/ -name "*.js" -exec bash -c 'mv "$1" "${1%.js}".mjs' - '{}' \;
