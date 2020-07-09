400bbda5e7b6ccb0d8835a24bf00ddf44f028c8e
nthypes <aasm.moura@gmail.com>
2020-07-09 10:49:36 -0300

    tests(runtime): add invalid team check

--- tests/runtime/1Examples/Sports.test.ts
+++ tests/runtime/1Examples/Sports.test.ts
@@ -10,6 +10,8 @@ import {
 } from 'src/runtime/index';
 import { TypeOf } from '@runtime/defs';
 import { isSuccessOf } from '@algebraic/types/Validation/Functions';
+import { Validation } from '@algebraic/index';
+import { identity } from '@common/identity';
 
 const League = Union (
   Literal ('NFL'),
@@ -52,7 +54,7 @@ it ('Should pass with a valid team', () => {
   expect (isSuccessOf (isTeam, teamPayload)).toBeTruthy ();
 });
 
-it ('Should pass with an invalid team', () => {
+it ('Should fail with an invalid team', () => {
   const teamPayload = {
     name: true,
     yearFounded: '1974',
@@ -62,4 +64,38 @@ it ('Should pass with an invalid team', () => {
 
   const isTeam = Team.Δ.check (teamPayload);
   expect (isSuccessOf (isTeam, teamPayload)).toBeFalsy ();
+});
+
+it ('Should pass with correct error return with an invalid team', () => {
+  const teamPayload = {
+    name: null,
+    yearFounded: '1974',
+    league: 'Soccer',
+    type: 'other',
+  };
+
+  const isTeam = Team.Δ.check (teamPayload);
+  const isTeamRes = Validation.λ.fold (identity, identity) (isTeam);
+  expect (isTeamRes).toEqual ([
+    {
+        code: 'IS_STRING',
+        message: 'Expected string but found (null :: object)',
+        path: 'name'
+    },
+    {
+        code: 'IS_NUMBER',
+        message: 'Expected Number but found (1974 :: string)',
+        path: 'yearFounded'
+    },
+    {
+      code: 'IS_UNION',
+      message: 'Expected (NFL | MLB | NBA | WNBA) but found (Soccer :: string)',
+      path: 'league'
+    },
+    {
+      code: 'IS_LITERAL',
+      message: 'Expected literal (team :: string) but found (other :: string)',
+      path: 'type'
+    }
+  ]);
 });
