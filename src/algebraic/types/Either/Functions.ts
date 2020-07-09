c16d5197d7fe017d2f0a25c522fefffc3b2a8930
nthypes <aasm.moura@gmail.com>
2020-07-09 18:11:34 -0300

    feat(algebraic/Either): adds fold method

--- src/algebraic/types/Either/Functions.ts
+++ src/algebraic/types/Either/Functions.ts
@@ -1,6 +1,6 @@
 import type { Eitherλ, Rightλ, Leftλ } from './Either';
 
-export { isLeft, isRight };
+export { isLeft, isRight, fold };
 
 /**
  * TODO: Add comment
@@ -17,4 +17,12 @@ const isRight = <A, B>(p1: Eitherλ<A, B>): p1 is Rightλ<A, B> =>
   p1.λ.id === 'Right';
 
 
-
+/**
+ * TODO: Add comment
+ * @param p1
+ */
+const fold = <A, B, C>(
+  onLeft: (a: A) => C,
+  onRight: (b: B) => C
+) => (p1: Eitherλ<A, B>) =>
+  isLeft (p1) ? onLeft (p1.λ.value) : onRight (p1.λ.value);
--- src/algebraic/types/Either/index.ts
+++ src/algebraic/types/Either/index.ts
@@ -1,15 +1,16 @@
-import type { ApplicativeOf2 } from "@algebraic/defs/Applicative";
-import type { ApplyOf2 } from "@algebraic/defs/Apply";
-import type { FunctorOf2 } from "@algebraic/defs/Functor";
-import { of, Right, Left } from "./Applicative";
-import { ap } from "./Apply";
-import { Either$λ } from "./Either";
-import { map, mapU } from "./Functor";
+import type { ApplicativeOf2 } from '@algebraic/defs/Applicative';
+import type { ApplyOf2 } from '@algebraic/defs/Apply';
+import type { FunctorOf2 } from '@algebraic/defs/Functor';
+import { of, Right, Left } from './Applicative';
+import { ap } from './Apply';
+import { Either$λ } from './Either';
+import { map, mapU } from './Functor';
+import { fold } from './Functions';
 
 type EitherModule = ApplicativeOf2<Either$λ> &
   FunctorOf2<Either$λ> &
   ApplyOf2<Either$λ> & {
-    λ: { Left: typeof Left; Right: typeof Right };
+    λ: { Left: typeof Left; Right: typeof Right; fold: typeof fold };
   };
 
 const EitherModule: EitherModule = {
@@ -20,6 +21,7 @@ const EitherModule: EitherModule = {
     map,
     Left,
     Right,
+    fold
   },
   λU: {
     kind: Either$λ,
