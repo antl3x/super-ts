ae0bc2e22d7d8887dc811ffc3e90342dc5c01f6c
nthypes <aasm.moura@gmail.com>
2020-07-09 10:18:52 -0300

    chore(validation): adds fold method to module

--- src/algebraic/types/Validation/index.ts
+++ src/algebraic/types/Validation/index.ts
@@ -1,12 +1,13 @@
-import type { ApplicativeOf2 } from "@algebraic/defs/Applicative";
-import type { ApplyOf2 } from "@algebraic/defs/Apply";
-import type { FunctorOf2 } from "@algebraic/defs/Functor";
-import { Failure, of, Success } from "./Applicative";
-import { ap } from "./Apply";
-import { map, mapU, mapFailure } from "./Functor";
-import { Validation$λ } from "./Validation";
-import { ChainOf2 } from "@algebraic/defs/Chain";
-import { chainU, chain } from "./Chain";
+import type { ApplicativeOf2 } from '@algebraic/defs/Applicative';
+import type { ApplyOf2 } from '@algebraic/defs/Apply';
+import type { FunctorOf2 } from '@algebraic/defs/Functor';
+import { Failure, of, Success } from './Applicative';
+import { ap } from './Apply';
+import { map, mapU, mapFailure } from './Functor';
+import { Validation$λ } from './Validation';
+import { ChainOf2 } from '@algebraic/defs/Chain';
+import { chainU, chain } from './Chain';
+import { fold } from './Functions';
 
 type ValidationModule = ApplicativeOf2<Validation$λ> &
   FunctorOf2<Validation$λ> &
@@ -16,6 +17,7 @@ type ValidationModule = ApplicativeOf2<Validation$λ> &
       mapFailure: typeof mapFailure;
       Failure: typeof Failure;
       Success: typeof Success;
+      fold: typeof fold;
     };
   };
 
@@ -26,6 +28,7 @@ const ValidationModule: ValidationModule = {
     of,
     map,
     chain,
+    fold,
     mapFailure,
     Failure,
     Success,
