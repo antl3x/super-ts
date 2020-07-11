ffb7fc52895d7e8a692704e73f1b5219f0c810ef
nthypes <aasm.moura@gmail.com>
2020-07-11 13:02:51 -0300

    feat(algebraic/Maybe): adds fromNullable method

--- src/algebraic/types/Maybe/Functions.ts
+++ src/algebraic/types/Maybe/Functions.ts
@@ -1,6 +1,7 @@
 import type { Justλ, Maybeλ, Nothingλ } from './Maybe';
+import { Nothing, Just } from './Applicative';
 
-export { isNothing, isJust, fromMaybe };
+export { isNothing, isJust, fromMaybe, fromNullable };
 
 /**
  * TODO: Add comment
@@ -13,6 +14,13 @@ const isNothing = <A>(p1: Maybeλ<A>): p1 is Nothingλ => p1.λ.id === 'Nothing'
  * @param p1
  */
 const isJust = <A>(p1: Maybeλ<A>): p1 is Justλ<A> => p1.λ.id === 'Just';
+
+/**
+ * TODO: Add Comment
+ * @param p1 
+ */
+const fromNullable = <A>(a: A): Maybeλ<NonNullable<A>> => a == null ? Nothing : Just (a as NonNullable<A>)
+
 
 const fromMaybe = <A>(p1: A) => (p2: Maybeλ<A>): A =>
   isNothing (p2) ? p1 : p2.λ.value;
--- src/algebraic/types/Maybe/index.ts
+++ src/algebraic/types/Maybe/index.ts
@@ -1,10 +1,11 @@
-import type { ApplicativeOf1 } from "@algebraic/defs/Applicative";
-import { ApplyOf1 } from "@algebraic/defs/Apply";
-import { FunctorOf1 } from "@algebraic/defs/Functor";
-import { Just, Nothing, of } from "./Applicative";
-import { ap } from "./Apply";
-import { map, mapU } from "./Functor";
-import { Maybe$λ } from "./Maybe";
+import type { ApplicativeOf1 } from '@algebraic/defs/Applicative';
+import { ApplyOf1 } from '@algebraic/defs/Apply';
+import { FunctorOf1 } from '@algebraic/defs/Functor';
+import { Just, Nothing, of } from './Applicative';
+import { ap } from './Apply';
+import { map, mapU } from './Functor';
+import { Maybe$λ } from './Maybe';
+import { fromNullable } from './Functions';
 
 type MaybeModule = ApplicativeOf1<Maybe$λ> &
   ApplyOf1<Maybe$λ> &
@@ -12,6 +13,7 @@ type MaybeModule = ApplicativeOf1<Maybe$λ> &
     λ: {
       Nothing: typeof Nothing;
       Just: typeof Just;
+      fromNullable: typeof fromNullable;
     };
   };
 
@@ -23,6 +25,7 @@ const MaybeModule: MaybeModule = {
     ap,
     Just,
     Nothing,
+    fromNullable
   },
   λU: {
     kind: Maybe$λ,
