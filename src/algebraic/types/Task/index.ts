f5f4b9649c1ed7d4cb9d328b5fc868143c63ceb8
nthypes <aasm.moura@gmail.com>
2020-07-11 13:03:15 -0300

    fix(algebraic/Task): removes unnecessary typeof

--- src/algebraic/types/Task/index.ts
+++ src/algebraic/types/Task/index.ts
@@ -8,11 +8,7 @@ import { Task$λ } from './Task';
 import { ChainOf1 } from '@algebraic/defs/Chain';
 import { chain, chainU } from './Chain';
 
-type TaskModule =  {
-  λ: {
-    of: typeof of;
-  };
-} & ApplicativeOf1<Task$λ> &
+type TaskModule = ApplicativeOf1<Task$λ> &
   FunctorOf1<Task$λ> &
   ChainOf1<Task$λ> &
   ApplyOf1<Task$λ>;
