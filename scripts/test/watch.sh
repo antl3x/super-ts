7f8dcd1abe1881137d0f87a7375c331bef931179
nthypes <aasm.moura@gmail.com>
2020-07-11 15:35:56 -0300

    fix(tests): remove build step from tests watch

--- scripts/test/watch.sh
+++ scripts/test/watch.sh
@@ -7,4 +7,4 @@ set -e
 echo -e "👻 💉 ${bold}[funtypes/test]${normal} starting unit testing with jest (watch mode).."
 
 node_modules/.bin/nodemon -e ts -w src -w tests \
- -x 'pnpm run build && jest --config jest.config.js'
+ -x 'jest --config jest.config.js'
